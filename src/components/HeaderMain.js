0e434b486f0117b40b049ec2666465f1a7df27a0
Vijay Shanker V R <102456001+vrvijayshanker@users.noreply.github.com>
2023-04-18 21:41:51 +0530

    small font fix in header

--- src/components/HeaderMain.js
+++ src/components/HeaderMain.js
@@ -5,7 +5,7 @@ import hlogo from '../assets/images/hlogo.svg'
 
 const HeaderMain = () => {
   return (
-    <div className='header-main container-fluid p-4 sticky-top'>
+    <div className='header-main container-fluid p-4 sticky-top font-link'>
         <Link to ={'/'}><img src={hlogo} className='rounded float-start ' alt='Logo'/></Link>
         <div className='navlinks text-end fw-bold'>
           <Link to={'/what_we_do'} className='navlink link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover' >What We Do</Link>

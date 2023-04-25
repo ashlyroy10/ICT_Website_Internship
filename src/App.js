78f22363738c01fd9d3d78cad355942d95efc60a
ANOOJ-BS <anoojbs97@gmail.com>
2023-04-25 13:34:12 +0530

    Changes by Anooj

--- src/App.js
+++ src/App.js
@@ -12,17 +12,19 @@ import AddStaff from './components/AddStaff';
 import AddTestimonial from './components/AddTestimonial';
 import SidePanel1 from './components/SidePanel1';
 import Courselist from './pages/Courselist';
-
+import Coursesection from './pages/Coursesection';
 
 function App() {
   return (
     <div className="App">
 
       {/* <SidePanel1/> */}
-      <Courselist/>
+      {/* <Courselist/> */}
+      {/* <Home/> */}
+      {/* <Coursesection/> */}
       
   
-     {/* <Routes>
+      <Routes>
      <Route path='/' element={<Home/>}/>
       <Route path='/what_we_do' element={<WhatWeDo/>}/>
       <Route path='/who_we_are' element={<WhoWeAre/>}/>
@@ -33,8 +35,9 @@ function App() {
       <Route path='/admindashboard/addcourse' element={<AddCourse/>} />
       <Route path='/admindashboard/addstaff' element={<AddStaff/>} /> 
       <Route path='/admindashboard/addtestimonials' element={<AddTestimonial/>} /> 
+      
     
-     </Routes> */}
+     </Routes> 
      
     </div>
   );

6d4381281f5e18244ab0ee77d64057e82464e356
ANOOJ-BS <anoojbs97@gmail.com>
2023-04-25 13:47:22 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import HeaderMain from '../components/HeaderMain'
-import Intropart from '../pages/Intropart'
-import Footer from '../components/Footer'
-import Testimonialcarousel from '../components/Testimonialcarousel'
-import Introcard from '../components/Introcard'
-import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
-      
-      <HeaderMain/>
-      <br></br><br></br><br></br>
-      <h1>ICTAK</h1>
+    <div className='row'>
+      <div>
+        <HeaderMain/>
+      </div>
     </div>
+
+    <div className='row'>
+      <Intropart/>
+    </div>
+
+    <div className='row'>
+      <Testimonialcarousel />
+    </div>
+
+    <div className='row'>
+      <Introcard />
+    </div>
+
+    <div className='row'>
+    <Footer/>
+    </div>
+  </div>
   )
 }
 

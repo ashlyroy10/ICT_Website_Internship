bca533f92e639b223bb9fe932cc49833971152ff
Vijay Shanker V R <102456001+vrvijayshanker@users.noreply.github.com>
2023-05-02 13:42:00 +0530

    ADMIN - Login, session, timeout, logout done

--- src/pages/Admincourse.js
+++ src/pages/Admincourse.js
@@ -55,16 +55,16 @@ const Admincourse = () => {
 
       {/* Course in a Card */}
 
-      <div class="container border-0">
-        <div class="row row-cols-1 row-cols-md-2 g-4">
-          <div class="col">
+      <div className="container border-0">
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+          <div className="col">
               
               
               {courseList.map((value, index) => (
-                <div class="card mb-3">
+                <div className="card mb-3" key={index}>
                 <div>
-                  <div className="card-header bg-secondary text-white" key={index}>
-                    <h5 class="card-title">{value.coursetitle}</h5>
+                  <div className="card-header bg-secondary text-white" >
+                    <h5 className="card-title">{value.coursetitle}</h5>
                     <p></p>
                   </div>
 
@@ -72,11 +72,11 @@ const Admincourse = () => {
 
                   {/* ACCORDION */}
 
-                  <div class="accordion accordion-flush" id={`accordianParent${index}`}>
-                    <div class="accordion-item">
-                      <h2 class="accordion-header">
+                  <div className="accordion accordion-flush" id={`accordianParent${index}`}>
+                    <div className="accordion-item">
+                      <h2 className="accordion-header">
                         <button
-                          class="accordion-button collapsed"
+                          className="accordion-button collapsed"
                           type="button"
                           data-bs-toggle="collapse"
                           data-bs-target={`#flush-collapseOne${index}`}
@@ -88,16 +88,16 @@ const Admincourse = () => {
                       </h2>
                       <div
                         id={`flush-collapseOne${index}`}
-                        class="accordion-collapse collapse"
+                        className="accordion-collapse collapse"
                         data-bs-parent={`#accordianParent${index}`}
                       >
-                        <div class="accordion-body">{value.overview}</div>
+                        <div className="accordion-body">{value.overview}</div>
                       </div>
                     </div>
-                    <div class="accordion-item">
-                      <h2 class="accordion-header">
+                    <div className="accordion-item">
+                      <h2 className="accordion-header">
                         <button
-                          class="accordion-button collapsed"
+                          className="accordion-button collapsed"
                           type="button"
                           data-bs-toggle="collapse"
                           data-bs-target={`#flush-collapseTwo${index}`}
@@ -109,16 +109,16 @@ const Admincourse = () => {
                       </h2>
                       <div
                         id={`flush-collapseTwo${index}`}
-                        class="accordion-collapse collapse"
+                        className="accordion-collapse collapse"
                         data-bs-parent={`#accordianParent${index}`}
                       >
-                        <div class="accordion-body">{value.description}</div>
+                        <div className="accordion-body">{value.description}</div>
                       </div>
                     </div>
-                    <div class="accordion-item">
-                      <h2 class="accordion-header">
+                    <div className="accordion-item">
+                      <h2 className="accordion-header">
                         <button
-                          class="accordion-button collapsed"
+                          className="accordion-button collapsed"
                           type="button"
                           data-bs-toggle="collapse"
                           data-bs-target={`#flush-collapseThree${index}`}
@@ -130,10 +130,10 @@ const Admincourse = () => {
                       </h2>
                       <div
                         id={`#flush-collapseThree${index}`}
-                        class="accordion-collapse collapse"
+                        className="accordion-collapse collapse"
                         data-bs-parent={`#accordianParent${index}`}
                       >
-                        <div class="accordion-body">
+                        <div className="accordion-body">
                           <small className="text-muted">
                             <p>Course Duration: {value.duration} hours</p>
                             <p>Internship: {value.internship} hours</p>
@@ -143,10 +143,10 @@ const Admincourse = () => {
                       </div>
                     </div>
 
-                    <div class="accordion-item">
-                      <h2 class="accordion-header">
+                    <div className="accordion-item">
+                      <h2 className="accordion-header">
                         <button
-                          class="accordion-button collapsed"
+                          className="accordion-button collapsed"
                           type="button"
                           data-bs-toggle="collapse"
                           data-bs-target={`#flush-collapseFour${index}`}
@@ -158,10 +158,10 @@ const Admincourse = () => {
                       </h2>
                       <div
                         id={`flush-collapseFour${index}`}
-                        class="accordion-collapse collapse"
+                        className="accordion-collapse collapse"
                         data-bs-parent={`#accordianParent${index}`}
                       >
-                        <div class="accordion-body">
+                        <div className="accordion-body">
                           <small className="text-muted">
                             <p>Mode of Study: {value.cmode}</p>
                             <p>Registrations open from {value.startdate} to {value.enddate}</p>
@@ -181,7 +181,7 @@ const Admincourse = () => {
                         width="16"
                         height="16"
                         fill="currentColor"
-                        class="bi bi-pencil-fill"
+                        className="bi bi-pencil-fill"
                         viewBox="0 0 16 16"
                       >
                         <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
@@ -199,7 +199,7 @@ const Admincourse = () => {
                         width="16"
                         height="16"
                         fill="currentColor"
-                        class="bi bi-trash3-fill"
+                        className="bi bi-trash3-fill"
                         viewBox="0 0 16 16"
                       >
                         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />

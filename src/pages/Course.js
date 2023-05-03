39ea960850163c4ab48f847bf4932e2f7a5ad0d3
Ashly Roy <ashlyroy10@gmail.com>
2023-05-03 12:27:15 +0530

    course page accordion

--- src/pages/Course.js
+++ src/pages/Course.js
@@ -1,122 +1,155 @@
 import React from 'react'
+import '../assets/css/style.css'
 import pythoncourse from '../assets/images/coursepic.png'
 
 const course = () => {
   return (
     
     <div class="container">
-    <br/>
-    <h2 class="text-start">Python Programming</h2>
-    
-
-    <button type="button" class="btn btn-danger float-start">Starts on 2023 May 15</button>
-    <br/>
-    <br/>
-    <br/>
-    <div class="row">
-      <div class="col-md-8">
-        <img src={pythoncourse} alt="python course pic" class="float-start" width="650" height="350"/>
-      <div/>
-
-        {/* <div class="col-md-8">  */}
-        
-        
-        
-        <h4 class="text-start">Overview</h4>
-  
-        <p className='text-justify'>Python is a general-purpose, object-oriented, high-level programming language. This program focuses on the installation of Python, the basic construct of Python, the basics of databases and use of Python in web development. Python has an enormous degree of implicit data structures and these make it attractive for Web Application Development. This program also covers the deployment of a web application.
-        </p>
-  
-        <h4 class="text-start">Scope of the Program</h4>
-  
-         <ul>
-                  <li class="text-start">Familiarise with basics of Python Programming</li>
-                  <li class="text-start">Familiarise with Database Concepts</li>
-                  <li class="text-start">Develop and deploy Web Applications using Python</li>
-                  
-                </ul>
-        <h4 class="text-start">Objectives</h4>
-  
-         <ul>
-                  <li class="text-start">Python is ranked number one in the IEEE spectrum ranking of Programming Languages in 2020.</li>
-                  <li class="text-start">Opportunities in the career of python are increasing tremendously in the world. </li>
-                  <li class="text-start">Whether you work in artificial intelligence or finance or are pursuing a career in web development or data science, Python is one of the most important skills you can learn.</li>
-                  <li class="text-start">Job roles advanced in python with high promising pay in large companies.</li>
-                  
-                </ul>
-        
-                <h4 class="text-start">Highlights</h4>
-  
-  <ul>
-           <li class="text-start">Suitable for graduates and working professionals.</li>
-           <li class="text-start">75% scholarship for meritorious candidates (Keralites). </li>
-           <li class="text-start">Access to 14000+ LinkedIn courses.</li>
-           <li class="text-start">Employability skills training.</li>
-           <li class="text-start">Virtual Internship.</li>
-         </ul>
-
-                <h4 class="text-start">Eligibility</h4>
-  
-  <ul>
-           <p class="text-start">The following shall be eligible to join the course;</p>
-           
-           <li class="text-start">Engineering or science graduates / three-year diploma holders in any engineering branches, final year/pre-final year students of these courses, having a foundation level knowledge (plus two equivalent) in Mathematics and Computer fundamental skills.</li>
-           <li class="text-start">Students who have completed their graduation but are awaiting the final results can also apply. </li>
-          
-         </ul>
-           <p class="text-start">*Please note that the ICT Academy of Kerala will have the right to cancel the candidature at any point if found ineligible.</p>
-
-                <h4 class="text-start">Course Agenda</h4>
-  
-  <ul>
-           <li class="text-start">Introduction to Python Programming</li>
-           <li class="text-start">Python Variables </li>
-           <li class="text-start">Data Structures</li>
-           <li class="text-start">Control Structures</li>
-           <li class="text-start">Functions in Python</li>
-           <li class="text-start">Introduction to Web development</li>
-           <li class="text-start">Basics of Database</li>
-           <li class="text-start">Web Application Development and Deployment</li>
-           
-         </ul>
-
-         <h4 class="text-start">Course Fee</h4>
-  
-  <ul>
-           <li class="text-start">Suitable for graduates and working professionals.</li>
-           <li class="text-start">75% scholarship for meritorious candidates (Keralites). </li>
-           <li class="text-start">Access to 14000+ LinkedIn courses.</li>
-           <li class="text-start">Employability skills training.</li>
-           <li class="text-start">Virtual Internship.</li>
-         </ul>
-
-      </div>
-      <div class="col-md-4">
-       <div class="side-info">
-       <button type = "button" class = "btn btn-outline-primary btn-lg fw-medium">8000 INR+GST</button>       <br/>
-       
-       <br/>
-       <span class="badge rounded-pill text-bg-success fs-5 fw-normal">OPEN</span>
-       <br/>
-       <br/>
-                <h3 class="text-center">This course includes</h3>
-                <ul>
-                  <li class="text-center"><strong>45 Days(105 Hrs)</strong></li>
-                  <li class="text-center"><strong>45 Hrs</strong></li>
-                  <li class="text-center"><strong>Hybrid Classes</strong></li>
-                  </ul>
-              </div>
-               
-            </div>
-  
-          </div>
-  
+      <br/>
+      <h2 class="text-start">Python Programming</h2>
+      <button type="button" class="btn btn-danger float-start">Starts on 2023 May 15</button><br/><br/><br/>
+      <div class="row">
+        <div class="col-md-8">
+          <img src={pythoncourse} alt="python course pic" class="float-start" width="800" height="450"/>
         </div>
-        // </div>
-      
-    
-      
-    
+        <div class="col-md 3">
+          <div class="side-info mt-5">
+            <button type = "button" class = "btn btn-outline-primary btn-lg fw-medium">8000 INR+GST</button><br/><br/>
+            <span class="badge rounded-pill text-bg-success fs-5 fw-normal">OPEN</span><br/><br/>
+            <h3 class="text-center">This course includes</h3>
+            <ul>
+              <li class="text-center"><strong>45 Days(105 Hrs)</strong></li>
+              <li class="text-center"><strong>45 Hrs</strong></li>
+              <li class="text-center"><strong>Hybrid Classes</strong></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="row" >
+        <div class="col-md-12">
+          <div className="course-container container font-link">
+            <div className="course-box bg-light">
+              <div class="accordion accordion-flush" id="accordionFlushExample">
+                <div class="accordion-item">
+                  <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                    <strong>Overview</strong></button>
+                  </h2>
+                  <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                      <div class="accordion-body">
+                          <p>Python is a general-purpose, object-oriented, high-level programming language. This program focuses on the installation of Python, the basic construct of Python, the basics of databases and use of Python in web development. Python has an enormous degree of implicit data structures and these make it attractive for Web Application Development. 
+                          This program also covers the deployment of a web application.</p>
+                      </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                    <strong>Scope of the Program</strong></button>
+                  </h2>
+                  <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                      <div class="accordion-body">
+                          <ul>
+                              <li class="text-start">Familiarise with basics of Python Programming</li>
+                              <li class="text-start">Familiarise with Database Concepts</li>
+                              <li class="text-start">Develop and deploy Web Applications using Python</li>
+                          </ul>
+                      </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                    <strong>Objectives</strong></button>
+                  </h2>
+                  <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                      <div class="accordion-body">
+                          <ul>
+                            <li class="text-start">Python is ranked number one in the IEEE spectrum ranking of Programming Languages in 2020.</li>
+                            <li class="text-start">Opportunities in the career of python are increasing tremendously in the world. </li>
+                            <li class="text-start">Whether you work in artificial intelligence or finance or are pursuing a career in web development or data science, Python is one of the most important skills you can learn.</li>
+                            <li class="text-start">Job roles advanced in python with high promising pay in large companies.</li> 
+                          </ul> 
+                      </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+                    <strong>Highlights</strong></button>
+                  </h2>
+                  <div id="flush-collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                      <ul>
+                          <li class="text-start">Suitable for graduates and working professionals.</li>
+                          <li class="text-start">75% scholarship for meritorious candidates (Keralites). </li>
+                          <li class="text-start">Access to 14000+ LinkedIn courses.</li>
+                          <li class="text-start">Employability skills training.</li>
+                          <li class="text-start">Virtual Internship.</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+                    <strong>Eligibility</strong></button>
+                  </h2>
+                  <div id="flush-collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                      <p class="text-start">The following shall be eligible to join the course;</p>
+                      <ul>
+                        <li class="text-start">Engineering or science graduates / three-year diploma holders in any engineering branches, final year/pre-final year students of these courses, having a foundation level knowledge (plus two equivalent) in Mathematics and Computer fundamental skills.</li>
+                        <li class="text-start">Students who have completed their graduation but are awaiting the final results can also apply. </li>
+                      </ul>
+                      <p class="text-start">*Please note that the ICT Academy of Kerala will have the right to cancel the candidature at any point if found ineligible.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
+                    <strong>Course Agenda</strong></button>
+                  </h2>
+                  <div id="flush-collapseSix" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                      <ul>
+                        <li class="text-start">Introduction to Python Programming</li>
+                        <li class="text-start">Python Variables </li>
+                        <li class="text-start">Data Structures</li>
+                        <li class="text-start">Control Structures</li>
+                        <li class="text-start">Functions in Python</li>
+                        <li class="text-start">Introduction to Web development</li>
+                        <li class="text-start">Basics of Database</li>
+                        <li class="text-start">Web Application Development and Deployment</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
+                    <strong>Course Fee</strong></button>
+                  </h2>
+                  <div id="flush-collapseSeven" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                      <ul>
+                        <li class="text-start">Suitable for graduates and working professionals.</li>
+                        <li class="text-start">75% scholarship for meritorious candidates (Keralites). </li>
+                        <li class="text-start">Access to 14000+ LinkedIn courses.</li>
+                        <li class="text-start">Employability skills training.</li>
+                        <li class="text-start">Virtual Internship.</li>
+                      </ul> 
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+    </div>
   )
 }
 
